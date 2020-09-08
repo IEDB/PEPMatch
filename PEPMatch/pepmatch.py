dcad019ad909a70daecef39e5adb868f374ba378
dmarrama <dmarrama@lji.org>
2020-09-08 14:17:35 -0700

    add comments throughout PEPMatch code

--- PEPMatch/pepmatch.py
+++ PEPMatch/pepmatch.py
@@ -12,14 +12,21 @@ splits = []
 database = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'proteomes/pepmatch.db')
 # database = '/run/media/dan/HDD/All/Projects/PEPMatch/Proteomes/pepmatch.db'
 
-
 def parse_fasta(file):
     '''Return a parsed Biopython SeqRecord object from a FASTA file.'''
     return SeqIO.parse(file, 'fasta')
 
-
 class Preprocessor(object):
+    '''
+    Object class that takes in a proteome FASTA file, k-size split, and format which
+    then preprocesses the proteome. With the preprocess method, it will break the proteome
+    into equal size k-mers and map them to locations within the individual proteins. The
+    mapped keys and values will then be stored in either pickle files or a SQLite database.
+    Optional: protein IDs can be versioned, so the versioned_protein_ids argument can be passed
+    as True to store them as versioned.
+    '''
     def __init__(self, proteome, split, preprocess_format, versioned_protein_ids = False):
+        # does not do 1-mer splits
         if split < 2:
             raise ValueError('k-sized split is invalid. Cannot be less than 2.')
 
@@ -53,7 +60,7 @@ class Preprocessor(object):
         with open(name + '_kmers' + '_' + str(self.split) + '.pickle', 'wb') as f:
             pickle.dump(kmer_dict, f)
 
-        # Dup protein names dictionary to a pickle proteome
+        # Dump protein names dictionary to a pickle proteome
         with open(name + '_names.pickle', 'wb') as f:
             pickle.dump(names_dict, f)
 
@@ -61,27 +68,32 @@ class Preprocessor(object):
         '''
         Takes the preprocessed proteome (below) and creates SQLite tables for both the 
         k-mer and names dictionaries created. These SQLite tables can then be used 
-        for searching. This is sometimes faster than using pickle files for the dictionaries.
+        for searching. This is much faster for exact matching.
         '''
         name = self.proteome.replace('.fa', '').split('/')[-1]
 
+        # make table names based on proteome and split size
         kmers_table = name + '_kmers' + '_' + str(self.split)
         names_table = name + '_names'
 
         conn = sqlite3.connect(database)
         c = conn.cursor()
 
+        # create tables if they do not already exist
         c.execute('CREATE TABLE IF NOT EXISTS "{k}"(kmer TEXT, position INT)'.format(k = kmers_table))
         c.execute('CREATE TABLE IF NOT EXISTS "{n}"(protein_number INT, protein_ID TEXT)'.format(n = names_table))
 
+        # make a row for each unique k-mer and position mapping
         for kmer, positions in kmer_dict.items():
             for position in positions:
                 c.execute('INSERT INTO "{k}" (kmer, position) VALUES (?, ?)'.format(k = kmers_table), (str(kmer), position,))
 
+        # make a row for each number to protein ID mapping
         for protein_number, protein_ID in names_dict.items():
             c.execute('INSERT INTO "{n}"(protein_number, protein_ID) VALUES(?, ?)'.format(n = names_table), 
                 (protein_number, protein_ID))
 
+        # create indexes for faster lookups
         c.execute('CREATE INDEX IF NOT EXISTS "{id}" ON "{k}"(kmer)'.format(id = kmers_table + '_id', k = kmers_table))
         c.execute('CREATE INDEX IF NOT EXISTS "{id}" ON "{n}"(protein_number)'.format(id = names_table + '_id', n = names_table))
 
@@ -99,7 +111,7 @@ class Preprocessor(object):
         to the protein ID to be read back later after searching.
         '''
 
-        # Read in proteome FASTA file and parse
+        # parse proteome using Biopython parse function
         proteome = parse_fasta(self.proteome)
 
         kmer_dict = {}
@@ -113,17 +125,16 @@ class Preprocessor(object):
             kmers = self.split_protein(str(protein.seq), self.split)
 
             for i in range(len(kmers)):
+
+                # if k-mer is already found in the dict, add position to value list
                 if kmers[i] in kmer_dict.keys():
-                    
-                    # append k-mer positions to the already established k-mer key
                     kmer_dict[kmers[i]].append(protein_count * 100000 + i)
                 
+                # if k-mer is not in dict, add it along with first list entry
                 else:
-
-                    # if k-mer has not been added to dictionary, add it first 
                     kmer_dict[kmers[i]] = [protein_count * 100000 + i]
 
-            # create protein keys by number to protein ID assignment
+            # create names mapping # to protein ID (include versioned if argument is passed) 
             if self.versioned_protein_ids:
                 protein_ID = str(protein.description).split(' ')[0]
                 names_dict[protein_count] = protein_ID.split('|',1)[0] + '|' + protein_ID.split('|')[1] + '.' + str(protein.description).split('SV=')[1] + '|' + protein_ID.split('|')[2]
@@ -132,12 +143,12 @@ class Preprocessor(object):
             
             protein_count += 1
 
+        # send the created dicts to be dumped into pickle files
         if self.preprocess_format == 'pickle':
-            # send the created dictionaries to be dumped into pickle files
             self.pickle_proteome(kmer_dict, names_dict)
         
+        # or send dicts to function to be logged into SQLite DB
         elif self.preprocess_format == 'sql':
-            # send the created dictionaries to be filled into a sql DB
             self.sql_proteome(kmer_dict, names_dict)
         
         else:
@@ -150,9 +161,12 @@ VALID_OUTPUT_FORMATS = ['csv', 'xlsx', 'json', 'html']
 
 class Matcher(Preprocessor):
     '''
-    Object that finds matches within a proteome from a query. This can be 
-    exact/mismatch/best match searching against the proteome.
-    The # of max mismatches can be specified in the initialization which will then.
+    Object class that inherits from Preprocessor. This is so queries can be preprocessed if
+    the stored objects don't already exist. The class takes in the query (either FASTA file 
+    or Python list), proteome file, and maximum # of mismatches. There are multiple methods
+    that will do exact/mismatch/best match searching against the proteome.
+    Optional: output and output_format arguments to write results to file. Supported formats
+    are "CSV", "XLSX", "JSON", and "HTML".
     '''
     def __init__(self, query, proteome, max_mismatches, output = False, output_format = 'csv'):
         if type(query) == list:
@@ -167,9 +181,12 @@ class Matcher(Preprocessor):
         self.output = output
         self.output_format = output_format
 
+        # use sql format for exact matches
         if max_mismatches == 0:
             self.split = min(self.lengths)
             self.preprocess_format = 'sql'
+        
+        # use pickle format for mismatching and calculate optimal k-splits by the query passed
         elif max_mismatches > 0:
             self.splits = self.mismatching_splits(self.lengths)
             self.split = self.splits[0]
@@ -220,6 +237,7 @@ class Matcher(Preprocessor):
         '''
         proteome_name = self.proteome.replace('.fa', '').split('/')[-1]
 
+        # make table names based on proteome and split size
         kmers_table_name = proteome_name + '_kmers' + '_' + str(self.split)
         names_table_name = proteome_name + '_names'
 
@@ -231,12 +249,16 @@ class Matcher(Preprocessor):
         all_matches_dict = {}
 
         for peptide in peptides:
+
+            # keep track of all matches and split peptide into k-mers
             all_matches_dict[peptide] = []
             kmers = self.split_peptide(peptide, self.split)
+            
             hit_list = []
 
-
             if len(peptide) % self.split == 0:
+
+                # lookup each k-kmer and add positions with offset of k-mer position in peptide
                 for i in range(0, len(kmers), self.split):
                     get_positions = 'SELECT position FROM "{kmer_table}" WHERE kmer = "{actual_kmer}"'.format(kmer_table = kmers_table_name, actual_kmer = kmers[i])
                     c.execute(get_positions)
@@ -247,12 +269,15 @@ class Matcher(Preprocessor):
                     except:
                         continue
 
+                # find where all k-mer positions agree on 1 location from their corresponding offset
+                # this will be a match if they all agree
                 sum_hits = Counter(hit_list)
                 for hit, count in sum_hits.items():
                     if count == len(peptide) // self.split:
                         all_matches_dict[peptide].append(hit)
-            
+
             else:
+                # start with first k-mer (0) look up then check i + k k-mer from there
                 i = 0
                 while i < len(peptide):
                     try:
@@ -267,6 +292,7 @@ class Matcher(Preprocessor):
                             continue
                         i += self.split
                     
+                    # if i + k k-mer is out of range of k-mers, just check the final k-mer
                     except IndexError:
                         get_positions = 'SELECT position FROM "{kmer_table}" WHERE kmer = "{actual_kmer}"'.format(kmer_table = kmers_table_name, actual_kmer = kmers[-1])
                         c.execute(get_positions)
@@ -277,7 +303,9 @@ class Matcher(Preprocessor):
                         except:
                             continue
                         i += self.split
-
+                
+                # find where all k-mer positions agree on 1 location from their corresponding offset
+                # this will be a match if they all agree
                 sum_hits = Counter(hit_list)
                 for hit, count in sum_hits.items():
                     if count == len(peptide) // self.split + 1:
@@ -285,11 +313,12 @@ class Matcher(Preprocessor):
 
         all_matches = []
 
-        # compile all matches into format used for benchmarking (tab separated)
+        # compile all matches into format used for benchmarking (comma separated)
         for peptide, matches in all_matches_dict.items():
             if matches == []:
                 all_matches.append((peptide, '', '', '', ''))
             for match in matches:
+                # retrieve protein IDs from the other created table
                 get_protein_name = 'SELECT protein_ID FROM "{names_table}" WHERE protein_number = "{protein_number}"'.format(
                                     names_table = names_table_name, protein_number = (match - (match % 100000)) // 100000)
                 c.execute(get_protein_name)
@@ -370,6 +399,11 @@ class Matcher(Preprocessor):
         return all_matches
 
     def mismatching_splits(self, lengths):
+        '''
+        There is an ideal k split for each peptide and max # of mismatches.
+        Some queries have many lengths, so we can calculate all the optimal k-size splits
+        we would need for optimal searching here.
+        '''
         splits = set()
         for length in lengths:
             if length // (self.max_mismatches + 1) not in [0,1]:
@@ -554,6 +588,11 @@ class Matcher(Preprocessor):
         return all_matches
 
     def best_match_splits(self, length):
+        '''
+        For best matching, we start with the lowest length then divide by half.
+        We continue to divide subsequent k by half until we reach k = 2. Then we 
+        use these splits for multiple lookups until we find a match for every peptide.
+        '''
         splits.append(length)
         if length > 3:
             return self.best_match_splits(length // 2)
@@ -564,14 +603,23 @@ class Matcher(Preprocessor):
             return splits
 
     def best_match(self):
+        '''
+        After calculating the splits we would need (starting with lowest peptide length 
+        in query), we can then call the mismatching function. We the maximum # of mismatches
+        calculated from the each length and split.
+        '''
         all_matches = []
 
         for split in self.splits:
             self.split = split
+
+            # calculate maximum possible # of mismatches that can guaranteed to be found
+            # using the lowest length and k-split
             self.max_mismatches = (min(self.lengths) // self.split)
 
             matches = self.mismatching()
 
+            # separate out peptides that did not match into a new query
             self.query = []
             for match in matches:
                 if match[1] != '':
@@ -579,11 +627,14 @@ class Matcher(Preprocessor):
                 else:
                     self.query.append(match[0])
 
+        # once we reach k = 2, we will just increase the # of mismatches until we find some
+        # match for each peptide in the query
         while self.query != []:
             self.max_mismatches += 1
 
             matches = self.mismatching()
 
+            # separate out peptides that did not match into a new query
             self.query = []
             for match in matches:
                 if match[1] != '':
@@ -594,9 +645,15 @@ class Matcher(Preprocessor):
         return all_matches
 
     def match(self):
+        '''
+        Overarching function that calls the appropriate search matching function
+        based on the parameters. 
+        '''
         if self.max_mismatches == 0:
             all_matches = self.exact_match()
         
+        # if we specify the # of mismatches, we can the use the optimal k-split
+        # given the lengths and # of mismatches
         elif self.max_mismatches > 0:
             all_matches = []
             query = self.query
@@ -611,6 +668,7 @@ class Matcher(Preprocessor):
         else:
             raise ValueError('Invalid input of mismatches.')
 
+        # option to output results to a specified format, default is CSV
         if self.output:
             df = self.dataframe_matches(all_matches)
             self.output_matches(df)
@@ -618,10 +676,12 @@ class Matcher(Preprocessor):
         return all_matches
 
     def dataframe_matches(self, all_matches):
+        '''Return Pandas dataframe of the results.'''  
         return pd.DataFrame(all_matches, 
             columns=['Peptide Sequence', 'Matched Peptide', 'Protein ID', 'Mismatches', 'Index start'])
 
     def output_matches(self, df):
+        '''Write Pandas dataframe to format that is specified'''
         if self.output_format == 'csv':
             return df.to_csv('PEPMatch_results.csv')
         elif self.output_format == 'xlsx':
@@ -632,6 +692,13 @@ class Matcher(Preprocessor):
             return df.to_html()
 
 class Benchmarker(Matcher):
+    '''
+    Object used for benchmarking the PEPMatch code for the various applications.
+    It uses three methods: 2 for preprocessing and 1 for searching as the benchmarking
+    code is structured. PEPMatch does not do any query preprocessing so it raises
+    a TypeError which is excepted in the benchmarking code.
+    Inherits from Matcher object.
+    '''
     def __init__(self, query, proteome, lengths, max_mismatches, algorithm_parameters):
         self.query = query
         self.proteome = proteome
@@ -645,9 +712,11 @@ class Benchmarker(Matcher):
         return 'PEPMatch'
 
     def preprocess_query(self):
+        '''No query preprocessing, raise TypeError'''
         raise TypeError(self.__str__() + ' does not preprocess queries.\n')
 
     def preprocess_proteome(self):
+        '''Preprocess proteome once or multiple times for each split calculated.'''
         if self.max_mismatches == 0:
             self.preprocess()
         else:
@@ -656,6 +725,10 @@ class Benchmarker(Matcher):
                 self.preprocess()
 
     def search(self):
+        '''
+        Call overarching match function. Then convert results into the standard format
+        needed to calculate accuracy.
+        '''
         matches = self.match()
 
         all_matches = []
@@ -672,6 +745,7 @@ class Benchmarker(Matcher):
 
             all_matches.append(match_string)
 
+        # remove files after benchmarking as the only purpose is to extract benchmarks
         try:
             os.remove(database)
         except FileNotFoundError:

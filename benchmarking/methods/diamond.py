ba012d812d92c0c175e4167f9d421fab419c412c
Daniel Marrama <dmarrama@lji.org>
2023-02-22 19:58:21 -0800

    text shifting corrections - fix MMseqs2 and DIAMOND idents and removals

--- benchmarking/methods/diamond.py
+++ benchmarking/methods/diamond.py
@@ -6,87 +6,87 @@ import os, glob
 directory = os.path.dirname(os.path.abspath(__file__))
 
 def parse_fasta(file):
-    return SeqIO.parse(file, 'fasta')
+  return SeqIO.parse(file, 'fasta')
 
 class DIAMOND(object):
-    def __init__(self, query, proteome, max_mismatches, method_parameters):
-        if max_mismatches == -1:
-            max_mismatches = 7
+  def __init__(self, query, proteome, max_mismatches, method_parameters):
+    if max_mismatches == -1:
+      max_mismatches = 7
 
-        self.query = query
-        self.proteome = proteome
-        self.proteome_name = proteome.replace('.fasta', '')
+    self.query = query
+    self.proteome = proteome
+    self.proteome_name = proteome.replace('.fasta', '')
 
-        self.max_mismatches = max_mismatches
+    self.max_mismatches = max_mismatches
 
-        bin_directory = method_parameters['bin_directory']
+    bin_directory = method_parameters['bin_directory']
 
-        self.bin_file = os.path.join(bin_directory, 'diamond')
+    self.bin_file = os.path.join(bin_directory, 'diamond')
 
-    def __str__(self):
-        return 'DIAMOND'
-    
-    def preprocesss(self):
-        os.system(self.bin_file + ' makedb --in ' + self.proteome + ' -d ' + self.proteome_name)
-    
-    def diamond_search(self, query, proteome):
-        os.system(self.bin_file + ' blastp -d ' + self.proteome_name + ' -q ' + self.query + ' -o matches.m8' + 
-                  ' -e 10000 -k 100 --ultra-sensitive --masking 0 -f 6 full_qseq sseq sseqid mismatch sstart')
+  def __str__(self):
+    return 'DIAMOND'
+  
+  def preprocesss(self):
+    os.system(self.bin_file + ' makedb --in ' + self.proteome + ' -d ' + self.proteome_name)
+  
+  def diamond_search(self, query, proteome):
+    os.system(self.bin_file + ' blastp -d ' + self.proteome_name + ' -q ' + self.query + ' -o matches.m8' + 
+              ' -e 10000 -k 100 --ultra-sensitive --masking 0 -f 6 full_qseq sseq sseqid mismatch sstart')
 
-        all_matches = []
-        with open('matches.m8', 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                match = []
-                result = line.split('\t')
-                for i in range(len(result)):
-                    if i == 4:
-                        match.append(int(result[i].replace('\n', '')) - 1)
-                    else:
-                        match.append(result[i])
+    all_matches = []
+    with open('matches.m8', 'r') as file:
+      lines = file.readlines()
+      for line in lines:
+        match = []
+        result = line.split('\t')
+        for i in range(len(result)):
+          if i == 4:
+            match.append(int(result[i].replace('\n', '')) - 1)
+          else:
+            match.append(result[i])
 
-                all_matches.append(match)
+        all_matches.append(match)
 
-        return all_matches
+    return all_matches
 
 
 class Benchmarker(DIAMOND):
-    def __init__(self, query, proteome, lengths, max_mismatches, method_parameters):
-        self.query = query
-        self.proteome = proteome
-        self.lengths = lengths
-        self.max_mismatches = max_mismatches
-        self.method_parameters = method_parameters
-        
-        super().__init__(query, proteome, max_mismatches, method_parameters)
+  def __init__(self, query, proteome, lengths, max_mismatches, method_parameters):
+    self.query = query
+    self.proteome = proteome
+    self.lengths = lengths
+    self.max_mismatches = max_mismatches
+    self.method_parameters = method_parameters
+    
+    super().__init__(query, proteome, max_mismatches, method_parameters)
 
-    def __str__(self):
-        return 'DIAMOND'
+  def __str__(self):
+    return 'DIAMOND'
 
-    def preprocess_proteome(self):
-        return self.preprocesss()
+  def preprocess_proteome(self):
+    return self.preprocesss()
 
-    def preprocess_query(self):
-        raise TypeError(self.__str__() + ' does not preprocess queries.\n')
+  def preprocess_query(self):
+    raise TypeError(self.__str__() + ' does not preprocess queries.\n')
 
-    def search(self):
-        matches = self.diamond_search(self.query, self.proteome)
+  def search(self):
+    matches = self.diamond_search(self.query, self.proteome)
 
-        all_matches = []
-        for match in matches:
-            match = list(match)
-            # try taking the UniProt ID - else do nothing 
-            try:
-                match[2] = match[2].split('|')[1]
-            except IndexError:
-                pass
-            all_matches.append(','.join([str(i) for i in match]))
+    all_matches = []
+    for match in matches:
+      match = list(match)
+      # try taking the UniProt ID - else do nothing 
+      try:
+        match[2] = match[2].split('|')[1]
+      except IndexError:
+        pass
+      all_matches.append(','.join([str(i) for i in match]))
 
-        # for extension in ['source', 'dbtype', 'index', 'idx', 'lookup', 'pot', 'pto']:
-        #     os.remove(glob.glob(os.path.dirname(self.proteome) + '/*.' + extension)[0])
+    # for extension in ['source', 'dbtype', 'index', 'idx', 'lookup', 'pot', 'pto']:
+    #     os.remove(glob.glob(os.path.dirname(self.proteome) + '/*.' + extension)[0])
 
-        os.remove('matches.m8')
-        os.remove(os.path.dirname(self.proteome) + '/%s.dmnd' % self.proteome_name.split('/')[-1])
-        # os.remove(os.path.dirname(self.proteome) + '/%s.dmnd' % self.proteome_name.split('/')[-1])
+    os.remove('matches.m8')
+    os.remove(os.path.dirname(self.proteome) + '/%s.dmnd' % self.proteome_name.split('/')[-1])
+    # os.remove(os.path.dirname(self.proteome) + '/%s.dmnd' % self.proteome_name.split('/')[-1])
 
-        return all_matches
+    return all_matches
--- benchmarking/methods/mmseqs2.py
+++ benchmarking/methods/mmseqs2.py
@@ -5,92 +5,92 @@ import os, glob, shutil
 directory = os.path.dirname(os.path.abspath(__file__))
 
 class MMseqs2(object):
-    def __init__(self, query, proteome, max_mismatches, method_parameters):
-        if max_mismatches == -1:
-            max_mismatches = 7
+  def __init__(self, query, proteome, max_mismatches, method_parameters):
+    if max_mismatches == -1:
+      max_mismatches = 7
 
-        self.query = query
-        self.proteome = proteome
-        self.proteome_name = proteome.replace('.fa', '')
+    self.query = query
+    self.proteome = proteome
+    self.proteome_name = proteome.replace('.fasta', '')
 
-        self.max_mismatches = max_mismatches
+    self.max_mismatches = max_mismatches
 
-        bin_directory = method_parameters['bin_directory']
-        self.bin_file = os.path.join(bin_directory, 'mmseqs')
+    bin_directory = method_parameters['bin_directory']
+    self.bin_file = os.path.join(bin_directory, 'mmseqs')
 
-    def __str__(self):
-        return 'MMseqs2'
-    
-    def preprocesss(self):
-        os.system(self.bin_file + ' createdb ' + self.proteome + ' ' + self.proteome_name)
-        os.system(self.bin_file + ' createindex ' + self.proteome_name + ' tmp')
-    
-    def mmseqs_search(self, query, proteome):
+  def __str__(self):
+    return 'MMseqs2'
+  
+  def preprocesss(self):
+    os.system(self.bin_file + ' createdb ' + self.proteome + ' ' + self.proteome_name)
+    os.system(self.bin_file + ' createindex ' + self.proteome_name + ' tmp')
+  
+  def mmseqs_search(self, query, proteome):
 
-        os.system(self.bin_file + ' easy-search ' + self.query + ' ' + self.proteome_name + ' results.m8' + 
-                  ' tmp' + ' -s 7.0' + ' -e 10000' + ' --format-output "qseq,taln,theader,mismatch,tstart"')
+    os.system(self.bin_file + ' easy-search ' + self.query + ' ' + self.proteome_name + ' results.m8' + 
+              ' tmp' + ' -s 7.0' + ' -e 10000' + ' --format-output "qseq,taln,theader,mismatch,tstart"')
 
-        all_matches = []
-        with open('results.m8', 'r') as file:
-            lines = file.readlines()
+    all_matches = []
+    with open('results.m8', 'r') as file:
+      lines = file.readlines()
 
-            for line in lines:
-                match = []
-                result = line.split('\t')
-                for i in range(len(result)):
-                    if i == 2:
-                        match.append(result[i].split(' ')[0])
-                    elif i == 4:
-                        match.append(int(result[i].replace('\n', '')) - 1)
-                    else:
-                        match.append(result[i])
+      for line in lines:
+        match = []
+        result = line.split('\t')
+        for i in range(len(result)):
+          if i == 2:
+            match.append(result[i].split(' ')[0])
+          elif i == 4:
+            match.append(int(result[i].replace('\n', '')) - 1)
+          else:
+            match.append(result[i])
 
-                all_matches.append(match)
+        all_matches.append(match)
 
-        return all_matches
+    return all_matches
 
 
 class Benchmarker(MMseqs2):
-    def __init__(self, query, proteome, lengths, max_mismatches, method_parameters):
-        self.query = query
-        self.proteome = proteome
-        self.lengths = lengths
-        self.max_mismatches = max_mismatches
-        self.method_parameters = method_parameters
-        
-        super().__init__(query, proteome, max_mismatches, method_parameters)
+  def __init__(self, query, proteome, lengths, max_mismatches, method_parameters):
+    self.query = query
+    self.proteome = proteome
+    self.lengths = lengths
+    self.max_mismatches = max_mismatches
+    self.method_parameters = method_parameters
+    
+    super().__init__(query, proteome, max_mismatches, method_parameters)
 
-    def __str__(self):
-        return 'MMseqs2'
+  def __str__(self):
+    return 'MMseqs2'
 
-    def preprocess_proteome(self):
-        return self.preprocesss()
+  def preprocess_proteome(self):
+    return self.preprocesss()
 
-    def preprocess_query(self):
-        raise TypeError(self.__str__() + ' does not preprocess queries.\n')
+  def preprocess_query(self):
+    raise TypeError(self.__str__() + ' does not preprocess queries.\n')
 
-    def search(self):
-        matches = self.mmseqs_search(self.query, self.proteome)
+  def search(self):
+    matches = self.mmseqs_search(self.query, self.proteome)
 
-        all_matches = []
-        for match in matches:
-            match = list(match)
-            # try taking the UniProt ID - else do nothing 
-            try:
-                match[2] = match[2].split('|')[1]
-            except IndexError:
-                pass
-            all_matches.append(','.join([str(i) for i in match]))
+    all_matches = []
+    for match in matches:
+      match = list(match)
+      # try taking the UniProt ID - else do nothing 
+      try:
+        match[2] = match[2].split('|')[1]
+      except IndexError:
+        pass
+      all_matches.append(','.join([str(i) for i in match]))
 
-        # remove all files generated by MMseqs2
-        for extension in ['source', 'dbtype', 'index', 'idx', 'lookup']:
-            for file in glob.glob(os.path.dirname(self.proteome) + '/*.' + extension):
-                os.remove(file)
-        os.remove(self.proteome.replace('.fasta', 'sta'))
-        os.remove(self.proteome.replace('.fasta', 'sta_h'))
-        os.remove('results.m8')
+    # remove all files generated by MMseqs2
+    for extension in ['source', 'dbtype', 'index', 'idx', 'lookup']:
+      for file in glob.glob(os.path.dirname(self.proteome) + '/*.' + extension):
+        os.remove(file)
+    os.remove(self.proteome.replace('.fasta', ''))
+    os.remove(self.proteome.replace('.fasta', '_h'))
+    os.remove('results.m8')
 
-        # remove tmp folder
-        shutil.rmtree('tmp')
+    # remove tmp folder
+    shutil.rmtree('tmp')
 
-        return all_matches
+    return all_matches

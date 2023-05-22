77959b07e59ab7a79e7d92fc86f4a526f90b4268
Daniel Marrama <dmarrama@lji.org>
2023-05-21 22:11:46 -0700

    replace unittest with pytest - exact match

--- test/test_exact_match.py
+++ test/test_exact_match.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import unittest
 import os
 
 import pandas as pd
@@ -8,43 +7,39 @@ import pandas.testing as pdt
 
 from pepmatch import Preprocessor, Matcher
 
-class TestExactMatch(unittest.TestCase):
-  def test_exact_match(self):
 
-    # paths
-    test_script_dir = os.path.dirname(os.path.realpath(__file__))
-    proteome_fasta = os.path.join(test_script_dir, '../benchmarking/proteomes/human.fasta')
-    query_fasta = os.path.join(test_script_dir, '../benchmarking/queries/mhc_ligands_test.fasta')
-    expected_csv = os.path.join(test_script_dir, '../benchmarking/expected/mhc_ligands_expected.csv')
+def test_exact_match():
+  # paths
+  test_script_dir = os.path.dirname(os.path.realpath(__file__))
+  proteome_fasta = os.path.join(test_script_dir, '../benchmarking/proteomes/human.fasta')
+  query_fasta = os.path.join(test_script_dir, '../benchmarking/queries/mhc_ligands_test.fasta')
+  expected_csv = os.path.join(test_script_dir, '../benchmarking/expected/mhc_ligands_expected.csv')
 
-    # preprocess human proteome
-    k=9
-    Preprocessor(proteome_fasta).sql_proteome(k)
+  # preprocess human proteome
+  k=9
+  Preprocessor(proteome_fasta).sql_proteome(k)
 
-    # match MHC ligands (9-mers) to human proteome
-    df = Matcher(
-      query=query_fasta,
-      proteome_file=proteome_fasta,
-      max_mismatches=0,
-      k=k,
-      output_format='dataframe').match()
+  # match MHC ligands (9-mers) to human proteome
+  df = Matcher(
+    query=query_fasta,
+    proteome_file=proteome_fasta,
+    max_mismatches=0,
+    k=k,
+    output_format='dataframe').match()
 
-    # remove preprocessed file
-    os.remove('human.db')
+  # remove preprocessed file
+  os.remove('human.db')
 
-    # load the expected data
-    expected_df = pd.read_csv(expected_csv)
+  # load the expected data
+  expected_df = pd.read_csv(expected_csv)
 
-    # select only the necessary columns to test for
-    df = df[['Query Sequence', 'Matched Sequence', 'Protein ID', 'Index start']]
-    expected_df = expected_df[['Query Sequence', 'Matched Sequence', 'Protein ID', 'Index start']]
+  # select only the necessary columns to test for
+  df = df[['Query Sequence', 'Matched Sequence', 'Protein ID', 'Index start']]
+  expected_df = expected_df[['Query Sequence', 'Matched Sequence', 'Protein ID', 'Index start']]
 
-    # sort dataframes by Query Sequence, Protein ID, and Index start
-    df = df.sort_values(by=['Query Sequence', 'Protein ID', 'Index start']).reset_index(drop=True)
-    expected_df = expected_df.sort_values(by=['Query Sequence', 'Protein ID', 'Index start']).reset_index(drop=True)
+  # sort dataframes by Query Sequence, Protein ID, and Index start
+  df = df.sort_values(by=['Query Sequence', 'Protein ID', 'Index start']).reset_index(drop=True)
+  expected_df = expected_df.sort_values(by=['Query Sequence', 'Protein ID', 'Index start']).reset_index(drop=True)
 
-    # assert dataframes are equal
-    pdt.assert_frame_equal(df, expected_df, check_dtype=False, check_exact=True)
-
-if __name__ == '__main__':
-  unittest.main()
+  # assert dataframes are equal
+  pdt.assert_frame_equal(df, expected_df, check_dtype=False, check_exact=True)

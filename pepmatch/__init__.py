c872616a2ddbeb5f60671a3c71bea404cd19859d
Daniel Marrama <dmarrama@lji.org>
2023-09-13 16:49:55 -0700

    include ParallelMatcher in package

--- pepmatch/__init__.py
+++ pepmatch/__init__.py
@@ -1,7 +1,8 @@
 from .preprocessor import Preprocessor
 from .matcher import Matcher
+from .parallel_match import ParallelMatcher
 from .benchmarker import Benchmarker
 
 from .version import __version__
 
-__all__ = ['Preprocessor', 'Matcher', 'Benchmarker', '__version__']
+__all__ = ['Preprocessor', 'Matcher', 'ParallelMatcher', 'Benchmarker', '__version__']

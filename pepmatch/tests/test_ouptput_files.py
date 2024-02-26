ffc5b2b79ac0f1673c1c1fc411990d2093a3c8c3
A G <02ninjakid@gmail.com>
2024-02-26 14:32:06 -0500

    tabs changed from four to two spaces

--- pepmatch/tests/test_ouptput_files.py
+++ pepmatch/tests/test_ouptput_files.py
@@ -11,68 +11,67 @@ from pepmatch.matcher import VALID_OUTPUT_FORMATS
 @pytest.fixture
 def match(proteome_path, query_path):
     return Matcher(
-            query=query_path,
-            proteome_file=proteome_path,
-            max_mismatches=0,
-            k=0,
-            output_format='csv'
-        )
+        query=query_path,
+        proteome_file=proteome_path,
+        max_mismatches=0,
+        k=0,
+        output_format='csv'
+    )
 
 
 # fixtures and supporting
 @pytest.fixture
 def simple_dataframe():
-    return DF(data={"col1" : [1, 2], "col2": [3, 4]})
+  return DF(data={"col1" : [1, 2], "col2": [3, 4]})
+
 
 def _creation_calls(_matcher, _path, _df):
-    if _path.exists():
-        remove(_path.__str__())
-
-    _matcher.output_name = _path
-    _matcher._output_matches(_df)
+  if _path.exists():
+    remove(_path.__str__())
+  _matcher.output_name = _path
+  _matcher._output_matches(_df)
 
 
 # Actual tests
 def test_local_creation(match, simple_dataframe):
-    """Test creation given the local path"""
-    local_location = Path("local_result.csv")
+  """Test creation given the local path"""
 
-    _creation_calls(match, local_location, simple_dataframe)
+  local_location = Path("local_result.csv")
+  _creation_calls(match, local_location, simple_dataframe)
 
-    assert local_location.exists()
-    remove(local_location.__str__())
+  assert local_location.exists()
+  remove(local_location.__str__())
 
 
 def test_relative_creation(match, simple_dataframe):
-    """Tests for creation given a relative path"""
-    relative_location = Path(f"../PEPMatch/relative.csv")
+  """Tests for creation given a relative path"""
 
-    _creation_calls(match, relative_location, simple_dataframe)
+  relative_location = Path(f"../PEPMatch/relative.csv")
+  _creation_calls(match, relative_location, simple_dataframe)
 
-    assert relative_location.exists()
-    remove(relative_location.__str__())
+  assert relative_location.exists()
+  remove(relative_location.__str__())
 
 
 def test_absolute_creation(match, simple_dataframe):
-    """Tests creation given an absolute path"""
-    absolute_location = Path("absolute.csv").absolute()
+  """Tests creation given an absolute path"""
 
-    _creation_calls(match, absolute_location, simple_dataframe)
+  absolute_location = Path("absolute.csv").absolute()
+  _creation_calls(match, absolute_location, simple_dataframe)
 
-    assert absolute_location.exists()
-    remove(absolute_location.__str__())
+  assert absolute_location.exists()
+  remove(absolute_location.__str__())
 
 
 def test_type_creation(match, simple_dataframe):
-    """Tests Matcher's ability to convert all supported types
-    into the expected file's output and format
-    """
-    for accepted_type in VALID_OUTPUT_FORMATS[1:]:
-        location = Path(f"test_output.{accepted_type}")
-        if location.exists():
-            remove(location.__str__())
-        match.output_format = accepted_type
-        _creation_calls(match, location, simple_dataframe)
-        assert location.exists()
-        remove(location.__str__())
-
+  """Tests Matcher's ability to convert all supported types
+  into the expected file's output and format
+  """
+  for accepted_type in VALID_OUTPUT_FORMATS[1:]:
+      location = Path(f"test_output.{accepted_type}")
+      if location.exists():
+          remove(location.__str__())
+      match.output_format = accepted_type
+      _creation_calls(match, location, simple_dataframe)
+      assert location.exists()
+      remove(location.__str__())
